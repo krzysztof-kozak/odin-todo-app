ca4ce0e8a483475416e32aed11f42c4e3c74bf5f
Kris <krzysztofkoz6@gmail.com>
2021-12-16 13:54:33 +0100

    Update components description

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,8 @@ Components:
         - has a list of TodoItems.
         - has a render method to draw DOM elements.
         - has an add/remove method to add/remove TodoItems to/from the list.
+        - has an edit method to edit TodoItem details.
+        - has a toggle status method to toggle TodoItem.status.
 
     2. Storage:
         - stores data using webstorage API.
@@ -18,9 +20,6 @@ Components:
 
     3. TodoItem:
         - has title, description, duedate, completed properites.
-        - has an edit method to update the details.
-        - has a toggleComplete method to toggle completed status.
-
 
     4. PubSub:
         - isolates any implementation details from all the involved components
@@ -28,7 +27,3 @@ Components:
         - allows components to subscribe to the events they care about.
         - broadcasts events to the interested subscribers.
 */
-
-const storage = new AppStorage();
-storage.set("APP_DATA", { name: "kris", age: 30, isMarried: false });
-console.log(storage.get("APP_DATA"));
