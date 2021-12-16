ea87482d88cf8a82cd3bd8ba5047f2b75e901d45
Kris <krzysztofkoz6@gmail.com>
2021-12-16 13:41:51 +0100

    Add AppStorage component

---
+++ src/js/components/AppStorage.js
@@ -0,0 +1,18 @@
+class AppStorage {
+	constructor() {
+		this.storage = localStorage;
+	}
+
+	get(key) {
+		const data = localStorage.getItem(key);
+		const formattedData = JSON.parse(data);
+		return formattedData;
+	}
+
+	set(key, value) {
+		const formattedValue = JSON.stringify(value);
+		localStorage.setItem(key, formattedValue);
+	}
+}
+
+export default AppStorage;
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,34 @@
 import "../css/reset.css";
 import "../css/style.css";
+
+import AppStorage from "./components/AppStorage";
+
+/* 
+Components:
+    1. App:
+        - is a main component of the app.
+        - has a list of TodoItems.
+        - has a render method to draw DOM elements.
+        - has an add/remove method to add/remove TodoItems to/from the list.
+
+    2. Storage:
+        - stores data using webstorage API.
+        - acts as a wrapper for the localstorage API.
+        - has get and set methods to update data in the storage.
+
+    3. TodoItem:
+        - has title, description, duedate, completed properites.
+        - has an edit method to update the details.
+        - has a toggleComplete method to toggle completed status.
+
+
+    4. PubSub:
+        - isolates any implementation details from all the involved components
+        - is responsible for communication between components.
+        - allows components to subscribe to the events they care about.
+        - broadcasts events to the interested subscribers.
+*/
+
+const storage = new AppStorage();
+storage.set("APP_DATA", { name: "kris", age: 30, isMarried: false });
+console.log(storage.get("APP_DATA"));
