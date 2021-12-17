569b17f8febdaa11e44e6a2efa3c23ee9aad7d31
Kris <krzysztofkoz6@gmail.com>
2021-12-17 07:24:31 +0100

    Move design notes to the readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # odin-todo-app
+
+## Components Design.
+
+    * App:
+        - is a main component of the app.
+        - has a list of TodoItems.
+        - has a render method to draw DOM elements.
+        - has an add/remove method to add/remove TodoItems to/from the list.
+        - has an edit method to edit TodoItem details.
+        - has a toggle status method to toggle TodoItem.status.
+
+    * Storage:
+        - stores data using webstorage API.
+        - acts as a wrapper for the localstorage API.
+        - has get and set methods to update data in the storage.
+
+    * Todo list:
+        - is a ui components.
+        - only cares about rendering a list of tasks in the DOM.
+        - subscribes to events that deal with adding or removing todos
+
+    * TodoItem:
+        - has title, description, duedate, completed properites.
+
+    * PubSub:
+        - isolates any implementation details from all the involved components
+        - is responsible for communication between components.
+        - allows components to subscribe to the events they care about.
+        - broadcasts events to the interested subscribers.
--- src/js/index.js
+++ src/js/index.js
@@ -5,31 +5,6 @@ import AppStorage from "./components/AppStorage";
 import App from "./components/App";
 import PublishSubscribe from "./components/PublishSubscribe";
 
-/* 
-Components:
-    1. App:
-        - is a main component of the app.
-        - has a list of TodoItems.
-        - has a render method to draw DOM elements.
-        - has an add/remove method to add/remove TodoItems to/from the list.
-        - has an edit method to edit TodoItem details.
-        - has a toggle status method to toggle TodoItem.status.
-
-    2. Storage:
-        - stores data using webstorage API.
-        - acts as a wrapper for the localstorage API.
-        - has get and set methods to update data in the storage.
-
-    3. TodoItem:
-        - has title, description, duedate, completed properites.
-
-    4. PubSub:
-        - isolates any implementation details from all the involved components
-        - is responsible for communication between components.
-        - allows components to subscribe to the events they care about.
-        - broadcasts events to the interested subscribers.
-*/
-
 const storage = new AppStorage();
 const app = new App(storage);
 app.render();
